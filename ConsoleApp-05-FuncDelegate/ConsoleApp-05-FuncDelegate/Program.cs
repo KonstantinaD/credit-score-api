0c1fcbf60e3beb85d5fabfb0dd18adbfebadc6ed
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-16 20:45:43 +0000

    Commit 05 - Func Delegate

---
+++ ConsoleApp-05-FuncDelegate/ConsoleApp-05-FuncDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp_05_FuncDelegate
+{
+    class Program
+    {
+        static Func<int, bool> isEqual = num => num == 5;
+        static void Main()
+        {
+            Console.WriteLine("Enter number: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            bool result = isEqual(num);
+
+            Console.WriteLine("The number is equal to 5: {0}", result);
+
+            Console.WriteLine("Press any key to exit.");
+
+            Console.ReadKey();
+        }
+    }
+}
