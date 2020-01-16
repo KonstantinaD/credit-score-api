030e547a33ba366a321dc834763d52caebd3a2f6
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-16 19:40:59 +0000

    Commit 05 - Delegate

---
+++ ConsoleApp-05-Delegate/ConsoleApp-05-Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp_05_Delegate
+{
+    class Program
+    {
+        delegate int squaringDel(int i);
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            squaringDel sqrDelegate = y => y * y;
+
+            int result = sqrDelegate(number);
+
+            Console.WriteLine("The squared number is: {0}", result);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+}
