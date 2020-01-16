409dddd4cd8d46a65a0e58ac5d6bf6d311204643
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-16 21:11:04 +0000

    Commit - 05 - Function Delegate 2

---
+++ ConsoleApp-05-FuncDelegate2/ConsoleApp-05-FuncDelegate2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp_05_FuncDelegate2
+{
+    class Program
+    {
+        static public bool CheckValue(int value)
+        {
+            return value + 5 == 10;
+        }
+
+        static Func<int, bool> value = num => num + 5 == 10;
+        static void Main()
+        {
+            Console.WriteLine("Enter number: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The result of the check using the method is: " + CheckValue(num));
+
+            Console.WriteLine();
+
+            Console.WriteLine("The result of the check using Func delegate is: " + value.Invoke(num));
+
+            Console.WriteLine("Press any key to exit.");
+
+            Console.ReadKey();
+        }
+    }
+}
