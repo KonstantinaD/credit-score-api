395082cdc9f47af69cb5b473f6988a90266bf91e
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-16 18:51:32 +0000

    Commit 05 - Lambda Expression

---
+++ ConsoleApp-05-Lambda/ConsoleApp-05-Lambda/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp_05_Lambda
+{
+    class Program
+    {
+        static int Square(int i)
+        {
+            int returnVal = i * i;
+            return returnVal;
+        }
+        static void Main()
+        {
+            int[] initialArray = { 2, 3, 4, 5 };
+
+            Console.WriteLine("Original Array Of Numbers: ");
+            Console.WriteLine(string.Join(" ", initialArray));
+
+            Console.WriteLine("Using Lambda Syntax: ");
+            var squaredArray = initialArray.Select(y => y * y);
+            Console.WriteLine(string.Join(" ", squaredArray));
+
+            Console.WriteLine("Calling a method: ");
+            foreach (int num in initialArray)
+            {
+                    Console.WriteLine("Number = " + Square(num));
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
