bb5c7e07b74b5e7f2d32403f19c7961c57b10817
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-17 11:03:39 +0000

    Commit 05 - Operator overloading Car

---
+++ ConsoleApp-05-OperatorOverloadingCar/ConsoleApp-05-OperatorOverloadingCar/Car.cs
@@ -0,0 +1,78 @@
+﻿namespace ConsoleApp_05_OperatorOverloadingCar
+{
+    class Car
+    {
+        private string _make;
+        private string _model;
+        private string _colour;
+        private double _price;
+        private string _registeredKeeper;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string carMake, string carModel, string carColour, double carPrice, string keeper)
+        {
+            this._make = carMake;
+            this._model = carModel;
+            this._colour = carColour;
+            this._price = carPrice;
+            this._registeredKeeper = keeper;
+        }
+
+        public string Make
+        {
+            get { return this._make; }
+            set { this._make = value; }
+        }
+
+        public string Model
+        {
+            get { return this._model; }
+            set { this._model = value; }
+        }
+
+        public string Colour
+        {
+            get { return this._colour; }
+            set { this._colour = value; }
+        }
+
+        public double Price
+        {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+
+        public string RegisteredKeeper
+        {
+            get { return this._registeredKeeper; }
+            set { this._registeredKeeper = value; }
+        }
+
+        public string GetDetails()
+        {
+            return _make + " " + _model + " " + _colour + " " + _price + " " + _registeredKeeper;
+        }
+
+        public static Car operator +(Car car1, Car car2)
+        {
+            Car newCar = new Car
+            {
+                Make = car1._make + " and " + car2._make,
+
+                Model = car1._model + " and " + car2._model,
+
+                Colour = car1._colour + " and " + car2._colour,
+
+                Price = car1._price + car2._price,
+
+                RegisteredKeeper = car1._registeredKeeper + " and " + car2._registeredKeeper
+            };
+
+            return newCar;
+        }
+    }
+}
---
+++ ConsoleApp-05-OperatorOverloadingCar/ConsoleApp-05-OperatorOverloadingCar/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp_05_OperatorOverloadingCar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car1 = new Car("Toyota", "Corolla", "red", 1000, "Oscar");
+            Car car2 = new Car("Ford", "Mondeo", "blue", 2000, "Benny");
+            Car car3 = new Car("Honda", "Yaris", "white", 3000, "Maria");
+            Car car4 = new Car("Ferrari", "Roche", "silver", 4000, "Natalia");
+            Car car5 = new Car("Mitsubishi", "Japa", "black", 5000, "Orka");
+            Car car6 = new Car("VW", "Passat", "gold", 6000, "Teddy");
+
+            Console.WriteLine("The details of car 1 are: " + car1.GetDetails());
+            Console.WriteLine("The details of car 2 are: " + car2.GetDetails());
+            Console.WriteLine("The details of car 3 are: " + car3.GetDetails());
+            Console.WriteLine("The details of car 4 are: " + car4.GetDetails());
+            Console.WriteLine("The details of car 5 are: " + car5.GetDetails());
+            Console.WriteLine("The details of car 6 are: " + car6.GetDetails());
+
+            Console.WriteLine();
+
+            Car newCarTwoCarsAdded1 = car1 + car2;
+            Console.WriteLine("The details of the new car (made up of the first 2 cars) are (note that the price is a sum of the prices of the added cars): " + newCarTwoCarsAdded1.GetDetails());
+
+            Console.WriteLine();
+
+            Car newCarTwoCarsAdded2 = car3 + car4;
+            Console.WriteLine("The details of the new car (made up of the second 2 cars) are (note that the price is a sum of the prices of the added cars): " + newCarTwoCarsAdded2.GetDetails());
+
+            Console.WriteLine();
+
+            Car newCarFourCarsAdded = newCarTwoCarsAdded1 + newCarTwoCarsAdded2;
+
+            Car newCarFiveCarsAdded = newCarFourCarsAdded + car5;
+            Console.WriteLine("The details of the new car (made up of the first 5 cars) are (note that the price is a sum of the prices of the added cars): " + newCarFiveCarsAdded.GetDetails());
+
+            Console.WriteLine("Press any key to exit.");
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
