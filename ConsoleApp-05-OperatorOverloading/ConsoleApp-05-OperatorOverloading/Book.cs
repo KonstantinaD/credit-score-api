c911f296c57f4cc54d25e6e132a3e8ec6eb7035c
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-17 08:52:41 +0000

    Commit 05-Operator Overloading again

---
+++ ConsoleApp-05-OperatorOverloading/ConsoleApp-05-OperatorOverloading/Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp_05_OperatorOverloading
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int numPages;
+
+        public Book() { }
+        public Book(string bookTitle, string bookAuthor, int numberOfPages)
+        {
+            title = bookTitle;
+            author = bookAuthor;
+            numPages = numberOfPages;
+        }
+
+        public string Title
+        {
+            get { return this.title;  }
+            set { this.title = value; }
+        }
+
+        public string Author
+        {
+            get { return this.author; }
+            set { this.author = value; }
+        }
+
+        public int NumPages
+        {
+            get { return numPages; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Number Of Pages cannot be zero/negative");
+                }
+                else
+                {
+                    numPages = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format(" {0}, {1}, {2} ", title, author, numPages);
+        }
+
+        // Overload + operator to add two book objects.
+        public static Book operator +(Book book1, Book book2)
+        {
+            Book newBook = new Book();
+            newBook.title = book1.title + " and " + book2.title;
+            newBook.author = book1.author + " and " + book2.author;
+            newBook.numPages = book1.numPages + book2.numPages;
+
+            return newBook;
+        }
+
+    }
+
+}
---
+++ ConsoleApp-05-OperatorOverloading/ConsoleApp-05-OperatorOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp_05_OperatorOverloading
+{
+    class Program
+    {
+        static void Main()
+        {
+            Book bookC = new Book("Microsoft Visual C#", "John Sharp", 150);
+            Book bookJava = new Book("Java 101", "Shara Sharp", 200);
+            Book newBook = new Book();
+
+            //displaying the Books 
+            Console.WriteLine("bookC : {0}", bookC.ToString());
+            Console.WriteLine("bookJava : {0}", bookJava.ToString());
+
+            newBook = bookC + bookJava;
+            Console.WriteLine("newBook: {0}", newBook.ToString());
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
