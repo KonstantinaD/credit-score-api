9cf517bed51e2ef462e7d6dc91e6e5188e4411b3
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-17 18:05:03 +0000

    Commit - 06 Calculator

---
+++ ConsoleApp-06-Calculator/ConsoleApp-06-Calculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp_06_Calculator
+{
+    class Calculator
+    {
+        internal static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        internal static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        internal static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        internal static double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new ArgumentException("Cannot divide by 0");
+            }
+
+            return num1 / num2;
+        }
+    }
+}
---
+++ ConsoleApp-06-Calculator/ConsoleApp-06-Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp_06_Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number 1: ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2: ");
+            double number2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The result of the Add() method is: " + Calculator.Add(number1, number2));
+            Console.WriteLine("The result of the Subtract() method is: " + Calculator.Subtract(number1, number2));
+            Console.WriteLine("The result of the Multiply() method is: " + Calculator.Multiply(number1, number2));
+            Console.WriteLine("The result of the Divide() method is: " + Calculator.Divide(number1, number2));
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
