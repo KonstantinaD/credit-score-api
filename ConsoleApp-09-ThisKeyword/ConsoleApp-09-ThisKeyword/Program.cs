eafe13eb782f9b4293fd1b9df3d4d505918c66ec
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-18 17:15:24 +0000

    Commit 09 ''This'' keyword

---
+++ ConsoleApp-09-ThisKeyword/ConsoleApp-09-ThisKeyword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp_09_ThisKeyword
+{
+    class Program
+    {
+        static void Main()
+        {
+            User user1 = new User();
+
+            Console.WriteLine("The default details of the user are: " + user1.GetDetails());
+
+            User user2 = new User("Mimi", "Por");
+
+            Console.WriteLine("The custom details of the user are: " + user2.GetDetails());
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp-09-ThisKeyword/ConsoleApp-09-ThisKeyword/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_09_ThisKeyword
+{
+	public class User
+	{
+
+		public String firstName;
+		public String lastName;
+		public User()
+				 : this("default_first", "default_last")
+		{
+		}
+
+		public User(String firstName, String lastName)
+		{
+
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+
+		public string GetDetails()
+		{
+			return firstName + " " + lastName;
+		}
+	}
+}
