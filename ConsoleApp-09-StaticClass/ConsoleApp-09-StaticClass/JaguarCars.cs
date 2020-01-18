7179c3203b67cff5d96871a631671b5832b7723e
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-18 15:11:27 +0000

    Commit 09 Static Class

---
+++ ConsoleApp-09-StaticClass/ConsoleApp-09-StaticClass/JaguarCars.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp_09_StaticClass
+{
+    public static class JaguarCars
+    {
+        //initialised at runtime
+        internal static readonly string companyName = "Jaguar";
+        internal static readonly string companyRegOffice;
+
+        //static constructor initialises field
+        static JaguarCars()
+        {
+            companyRegOffice = "Whitley, Coventry, England.";
+        }
+    }
+}
---
+++ ConsoleApp-09-StaticClass/ConsoleApp-09-StaticClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp_09_StaticClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("JaguarCars.companyName : " + JaguarCars.companyName);
+            Console.WriteLine("JaguarCars.companyRegOffice : " + JaguarCars.companyRegOffice);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
