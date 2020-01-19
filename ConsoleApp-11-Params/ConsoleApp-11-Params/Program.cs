6c59270526e53f13e50976bbeebd4158b421478d
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-19 12:08:58 +0000

    Commit 11 - Params

---
+++ ConsoleApp-11-Params/ConsoleApp-11-Params/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp_11_Params
+{
+    class Program
+    {
+        public static void ParamsMethod(params int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + (i < arr.Length - 1 ? ", " : ""));
+            }
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+            int[] arr = new int[] { 2, 5, 1, 6, 8, 3 };
+            ParamsMethod(arr);
+            Console.WriteLine("\nPress Any Key to Exit...");
+            Console.ReadKey();
+        }
+    }
+}
