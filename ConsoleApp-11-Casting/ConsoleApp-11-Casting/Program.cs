0bc676be2fd514765d6bb5b1afb8e8ab706a09bd
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-19 14:23:08 +0000

    Commit 11 - AutomaticAndExplicitConversion

---
+++ ConsoleApp-11-Casting/ConsoleApp-11-Casting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp_11_Casting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 23;
+            double d = 15.12;
+
+            //automatic type conversion 
+            long l = i;
+            float f = l;
+            float fl = i;
+
+            //explicit type casting
+            //returns the whole number part of the double
+            int doubleToInt = (int)d;
+
+            Console.WriteLine("Int value " + i);
+            Console.WriteLine("Long value from int " + l);
+            Console.WriteLine("Float value from long " + f);
+            Console.WriteLine("Float value from int " + fl);
+            Console.WriteLine("Int value from double " + doubleToInt);
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
