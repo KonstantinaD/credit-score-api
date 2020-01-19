d62d7380bdc5c2c108ce1344c5c595dc2f8ae2d2
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-19 15:33:25 +0000

    Commit 11 Parse vs TryParse

---
+++ ConsoleApp-11-ParseAndTryParse/ConsoleApp-11-ParseAndTryParse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp_11_ParseAndTryParse
+{
+    class Program
+    {
+        static void Main()
+        {
+            string validString = "False";
+            string invalidString = "boo";
+
+            bool result1 = bool.Parse(validString);
+            Console.WriteLine("The result of parsing a valid string (\"False\") to boolean is: " + result1);
+
+            bool result2 = bool.TryParse(invalidString, out bool typeToBeConvertedInto);
+            Console.WriteLine("\nParsing an invalid string to boolean succeeded?: " + result2);
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
