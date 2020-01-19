e04e7279ec7ec0dbb78587aa79169dde755a8083
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-19 11:05:54 +0000

    Commit 11 - Assert

---
+++ ConsoleApp-11-Assert/ConsoleApp-11-Assert/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp_11_Assert
+{
+    class Program
+    {
+        static int IntegerDivide(int dividend, int divisor)
+        {
+            Debug.Assert(divisor != 0, "Cannot divide by 0");
+            return (dividend / divisor);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter a dividend: ");
+            int dividend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a divisor: ");
+            int divisor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The result is: " + IntegerDivide(dividend, divisor));
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
