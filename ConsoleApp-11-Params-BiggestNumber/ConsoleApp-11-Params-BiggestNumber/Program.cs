72d9fe5cb94b64003d8edb697116a83ae5dc3ca9
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-19 13:11:21 +0000

    Commit 11 - Params - Biggest number in an array

---
+++ ConsoleApp-11-Params-BiggestNumber/ConsoleApp-11-Params-BiggestNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp_11_Params_BiggestNumber
+{
+    class Program
+    {
+        public static int FindBiggestNumber(params int[] arr)
+        { 
+            return arr.Max();
+        }
+
+        static void Main()
+        {
+            int[] intArray = { 2, 4, 3, 8, 10, 0, 5 };
+
+            Console.WriteLine("The biggest number giving an array to the method is: " + FindBiggestNumber(intArray));
+
+            Console.WriteLine();
+
+            Console.WriteLine("The biggest number giving a a comma-separated list to the method is: " + FindBiggestNumber(5, 2, 8, 0, 1, 12, 7));
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
