aba7fec6172faf7cf0c4e663ce23810a46126045
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-19 13:48:45 +0000

    Commit 11 Biggest Number

---
+++ ConsoleApp-11-BiggestNumber/ConsoleApp-11-BiggestNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp_11_BiggestNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 0;
+            Console.WriteLine("Enter the total number of integers");
+            count = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[count];
+
+            Console.WriteLine("Enter the numbers");
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int biggestNumber = numbers[0];
+
+            for (int j = 1; j < count; j++)
+            {
+                if (biggestNumber < numbers[j])
+                {
+                    biggestNumber = numbers[j];
+                }
+            }
+            Console.WriteLine("The biggest number is: " + biggestNumber);
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
