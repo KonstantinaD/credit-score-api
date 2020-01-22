f8f4046953c0d185d9f7ee21d24034033b7dcfe7
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-22 21:48:19 +0000

    Commit 12 TDD - Smallest number

---
+++ ClassLibrary-12-TDD-SmallestNumber/ClassLibrary-12-TDD-SmallestNumber-MainCode/SmallestNumber.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace ClassLibrary_12_TDD_SmallestNumber_MainCode
+{
+    public class SmallestNumber
+    {
+        public int GetSmallestNumber(params int[] num)
+        {
+            int returnValue = 0;
+
+            if (num.Length != 0)
+            {
+                returnValue = num.Min();
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ ClassLibrary-12-TDD-SmallestNumber/ClassLibrary-12-TDD-SmallestNumber-UnitTests/SmallestNumberTests.cs
@@ -0,0 +1,83 @@
+﻿using ClassLibrary_12_TDD_SmallestNumber_MainCode;
+using NUnit.Framework;
+
+namespace ClassLibrary_12_TDD_SmallestNumber_UnitTests
+{
+    public class SmallestNumberTests
+    {
+        [Test]
+        public void Num1Test_GetSmallestNumber_ExpectZero_WhenOnlyZeroIsSent()
+        {
+            //arrange
+            int num = 0;
+            SmallestNumber smallestNumber = new SmallestNumber();
+
+            //act
+            int actualValue = smallestNumber.GetSmallestNumber(num);
+            int expectedValue = 0;
+
+            //arrange
+            Assert.That(expectedValue, Is.EqualTo(actualValue));
+        }
+
+        [Test]
+        public void Num2Test_GetSmallestNumber_ExpectOne_WhenOnlyOneIsSent()
+        {
+            //arrange
+            int num = 1;
+            SmallestNumber smallestNumber = new SmallestNumber();
+
+            //act
+            int actualValue = smallestNumber.GetSmallestNumber(num);
+            int expectedValue = 1;
+
+            //arrange
+            Assert.That(expectedValue, Is.EqualTo(actualValue));
+        }
+
+        [Test]
+        public void Num3Test_GetSmallestNumber_ExpectZero_WhenEmptyListIsSent()
+        {
+            //arrange
+            int[] num = new int[] { };
+            SmallestNumber smallestNumber = new SmallestNumber();
+
+            //act
+            int actualValue = smallestNumber.GetSmallestNumber(num);
+            int expectedValue = 0;
+
+            //arrange
+            Assert.That(expectedValue, Is.EqualTo(actualValue));
+        }
+
+        [Test]
+        public void Num4Test_GetSmallestNumber_Expect1_When1And5IsSent()
+        {
+            //arrange
+            int[] num = new int[] {5, 1};
+            SmallestNumber smallestNumber = new SmallestNumber();
+
+            //act
+            int actualValue = smallestNumber.GetSmallestNumber(num);
+            int expectedValue = 1;
+
+            //arrange
+            Assert.That(expectedValue, Is.EqualTo(actualValue));
+        }
+
+        [Test]
+        public void Num5Test_GetSmallestNumber_ExpectMinus2_WhenListOfNumbersWithMinus2AsTheLowestIsSent()
+        {
+            //arrange
+            int[] num = new int[] { 7, -2, 0, 1, -1 };
+            SmallestNumber smallestNumber = new SmallestNumber();
+
+            //act
+            int actualValue = smallestNumber.GetSmallestNumber(num);
+            int expectedValue = -2;
+
+            //arrange
+            Assert.That(expectedValue, Is.EqualTo(actualValue));
+        }
+    }
+}
