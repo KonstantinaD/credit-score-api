1b0575bb62badb48a35c08ee58ac3e68c9e7ba00
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-23 08:59:43 +0000

    Commit 12 Fizz Buzz - further refactoring

--- ClassLibrary-12-TDD-FizzBuzz/ClassLibrary-12-TDD-FizzBuzz-MainCode/FizzBuzz.cs
+++ ClassLibrary-12-TDD-FizzBuzz/ClassLibrary-12-TDD-FizzBuzz-MainCode/FizzBuzz.cs
@@ -14,17 +14,21 @@ namespace ClassLibrary_12_TDD_FizzBuzz_MainCode
                 {
                     int integerValue = int.Parse(numbers[i]);
 
-                    if (integerValue % 3 == 0 && integerValue % 5 == 0)
+                    bool isMultipleOf3 = integerValue % 3 == 0;
+
+                    bool isMultipleOf5 = integerValue % 5 == 0;
+
+                    if (isMultipleOf3 && isMultipleOf5)
                     {
                         returnedArray[i] = "FIZZ BUZZ";
                     }
 
-                    else if (integerValue % 3 == 0)
+                    else if (isMultipleOf3)
                     {
                         returnedArray[i] = "FIZZ";
                     }
 
-                    else if (integerValue % 5 == 0)
+                    else if (isMultipleOf5)
                     {
                         returnedArray[i] = "BUZZ";
                     }
