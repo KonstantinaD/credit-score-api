d2c8ff0ceacd547502d7dfb5a0b913a3be1ed5b9
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-25 10:26:50 +0000

    Commit 14- TestCase - parametrised tests GetGrade

---
+++ 14_TestCase_ParametrisedTests_Grade/14_TestCase_ParametrisedTests_Grade_MainCode/Grade.cs
@@ -0,0 +1,30 @@
+﻿namespace _14_TestCase_ParametrisedTests_Grade_MainCode
+{
+    public class Grade
+    {
+        public static string GetGrade(int score)
+        {
+            string returnVal = "";
+
+            switch (score)
+            {
+                case int x when (x >= 95):
+                    returnVal = "Distinction";
+                    break;
+                case int x when (x >= 85):
+                    returnVal = "Merit";
+                    break;
+                case int x when (x >= 75):
+                    returnVal = "Pass";
+                    break;
+
+                default:
+                    returnVal = "Fail";
+                    break;
+            }
+
+            return returnVal;
+        }
+
+    }
+}
---
+++ 14_TestCase_ParametrisedTests_Grade/UnitTests/GradeTest.cs
@@ -0,0 +1,22 @@
+﻿using _14_TestCase_ParametrisedTests_Grade_MainCode;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class GradeTest
+    {
+        [TestCase(74, "Fail")]
+        [TestCase(75, "Pass")]
+        [TestCase(84, "Pass")]
+        [TestCase(85, "Merit")]
+        public void GetGrade_GetsRightGrade_ForScore(int score, string expectedGrade)
+        {
+            //act
+            string actualValue = Grade.GetGrade(score);
+
+            //assert
+            Assert.That(actualValue, Is.EqualTo(expectedGrade));
+        }
+    }
+}
