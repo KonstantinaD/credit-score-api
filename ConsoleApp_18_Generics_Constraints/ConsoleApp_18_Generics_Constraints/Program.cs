bccafdd85b96f286345aa630ce2763d95eeb2a9e
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-27 14:17:27 +0000

    Commit 18 - Generics - constraints on type parameters

---
+++ ConsoleApp_18_Generics_Constraints/ConsoleApp_18_Generics_Constraints/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp_18_Generics_Constraints
+{
+    class Program
+    {
+        public static void EqualsTest<T>(T s, T t) where T : class
+        {
+            System.Console.WriteLine(s.Equals(t));
+        }
+
+        static void Main()
+        {
+            string s1 = "target";
+            StringBuilder sb = new StringBuilder("target");
+            string s2 = sb.ToString();
+            EqualsTest<string>(s1, s2);
+
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
