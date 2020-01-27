087daca5a3cb56fa70b778ae592b136b7cf4ddfa
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-27 14:00:56 +0000

    Commit 18 - Generics

---
+++ ConsoleApp_18_Generics/ConsoleApp_18_Generics/GenericList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp_18_Generics
+{
+    class GenericList<T>
+    {  
+        public List<T> CreateList(T t)
+        {
+            List<T> list = new List<T>();
+
+            list.Add(t);
+
+            return list;
+        }
+    }
+}
---
+++ ConsoleApp_18_Generics/ConsoleApp_18_Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp_18_Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            var returnedIntegerList = new GenericList<int>().CreateList(5);
+            
+            Console.WriteLine("Integer list:");
+            for (int i = 0; i < returnedIntegerList.Count; i++)
+            {
+                Console.WriteLine(returnedIntegerList[i]);
+            }
+
+            var returnedDecimalList = new GenericList<decimal>().CreateList(12.56m);
+            Console.WriteLine("\nDecimal list:");
+            for (int i = 0; i < returnedDecimalList.Count; i++)
+            {
+                Console.WriteLine(returnedDecimalList[i]);
+            }
+
+            var returnedStringList = new GenericList<string>().CreateList("sample");
+            Console.WriteLine("\nString list:");
+            for (int i = 0; i < returnedStringList.Count; i++)
+            {
+                Console.WriteLine(returnedStringList[i]);
+            }   
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
