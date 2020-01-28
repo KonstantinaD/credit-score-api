9a23c49c681891745b5af1f47a559c900ae611c9
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-01-28 14:14:08 +0000

    Commit 19 - Collections, Overriding Equals() and GetHashCode()

---
+++ ConsoleApp_19_Collections_OverrideEqualsAndGetHashCode/ConsoleApp_19_Collections_OverrideEqualsAndGetHashCode/Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp_19_Collections_OverrideEqualsAndGetHashCode
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+
+
+        public Book(string iniTitle, string iniAuthor)
+        {
+            this.Title = iniTitle;
+            this.Author = iniAuthor;
+        }
+
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+
+        public string Author
+        {
+            get => author;
+            set => author = value;
+        }
+
+        public override bool Equals(object value)
+        {
+            // Is null?
+            if (Object.ReferenceEquals(null, value))
+            {
+                return false;
+            }
+
+            // Is the same object?
+            if (Object.ReferenceEquals(this, value))
+            {
+                return true;
+            }
+
+            // Is the same type?
+            if (value.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return IsEqual((Book)value);
+        }
+
+        private bool IsEqual(Book book)
+        {
+            // An implementation of value equality that use String.Equals 
+            return String.Equals(title, book.title)
+                    && String.Equals(author, book.author);
+        }
+
+        public override int GetHashCode() =>
+        new { title, author }.GetHashCode();
+    }
+}
+
+
+
---
+++ ConsoleApp_19_Collections_OverrideEqualsAndGetHashCode/ConsoleApp_19_Collections_OverrideEqualsAndGetHashCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp_19_Collections_OverrideEqualsAndGetHashCode
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine();
+            Book book1 = new Book("The Kubernetes Book", "Nigel P.");
+            Book book2 = new Book("The Kubernetes Book", "Nigel P.");
+            Book book3 = new Book("Docker Deep Dive", "Nigel P.");
+            Book book4 = new Book("Docker Deep Dive", "Nigel P.");
+
+            Console.WriteLine($"book1.Equals(book2) = { book1.Equals(book2) }");
+            Console.WriteLine($"book1.Equals(book3) = { book1.Equals(book3) }");
+
+            Console.WriteLine($"book3.Equals(book2) = { book3.Equals(book2) }");
+            Console.WriteLine($"book3.Equals(book4) = { book3.Equals(book4) }");
+
+            Console.WriteLine($"\nbook1 hash code = { book1.GetHashCode() }");
+            Console.WriteLine($"book2 hash code = { book2.GetHashCode() }");
+            Console.WriteLine($"book3 hash code = { book3.GetHashCode() }");
+            Console.WriteLine($"book4 hash code = { book4.GetHashCode() }");
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
