1a7ff60f7823d634b91f79214b285c57dddef34e
Konstantina Dureva <Konstantina.Dureva@fdmgroup.com>
2020-02-01 15:11:18 +0000

    Initial commit of min version

---
+++ score-api-project/ScoreAPI/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Customer
+    {
+        [Required]
+        [RegularExpression(@"^([a-zA-Z]+[ .'-]?)+([ ,]?)+[a-zA-Z]+.?$", ErrorMessage = "Please enter a valid name")]
+        public string Name {get; set;}
+    }
+}
